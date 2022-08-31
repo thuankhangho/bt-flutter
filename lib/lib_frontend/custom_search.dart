b1b4018054511858309889ac8e893847293f5199
VDTHP <dangcapnhat664@gmail.com>
2022-08-31 21:53:09 +0700

    seperate custom_search to a new file

---
+++ lib/lib_frontend/custom_search.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+class customSearch extends SearchDelegate {
+  List<String> allData = [
+    'am',
+    'is',
+    'are',
+    'be',
+    'china',
+    'russia',
+    'viet nam',
+    'banh mi',
+    'com tam',
+    'pho'
+  ];
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = '';
+        },
+      )
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back),
+      onPressed: () {
+        close(context, null);
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    List<String> matchQuery = [];
+    for (var item in allData) {
+      if (item.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(item);
+      }
+    }
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        var result = matchQuery[index];
+        return ListTile(
+          title: Text(result),
+        );
+      },
+      itemCount: matchQuery.length,
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    // TODO: implement buildResults
+    throw UnimplementedError();
+  }
+}
--- lib/lib_frontend/main_page.dart
+++ lib/lib_frontend/main_page.dart
@@ -4,70 +4,9 @@ import 'history.dart';
 import 'login.dart';
 import 'settings.dart';
 import 'package:flutter/widgets.dart';
+import 'custom_search.dart';
 
 TextEditingController search_input = TextEditingController();
-
-class customSearch extends SearchDelegate {
-  List<String> allData = [
-    'am',
-    'is',
-    'are',
-    'be',
-    'china',
-    'russia',
-    'viet nam',
-    'banh mi',
-    'com tam',
-    'pho'
-  ];
-
-  @override
-  List<Widget>? buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = '';
-        },
-      )
-    ];
-  }
-
-  @override
-  Widget? buildLeading(BuildContext context) {
-    return IconButton(
-      icon: Icon(Icons.arrow_back),
-      onPressed: () {
-        close(context, null);
-      },
-    );
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    List<String> matchQuery = [];
-    for (var item in allData) {
-      if (item.toLowerCase().contains(query.toLowerCase())) {
-        matchQuery.add(item);
-      }
-    }
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        var result = matchQuery[index];
-        return ListTile(
-          title: Text(result),
-        );
-      },
-      itemCount: matchQuery.length,
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    // TODO: implement buildResults
-    throw UnimplementedError();
-  }
-}
 
 class LineBoard extends StatelessWidget {
   const LineBoard(this.icon, this.title, this.onPressed, {super.key});
