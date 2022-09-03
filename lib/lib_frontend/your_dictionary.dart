912b027c2f2bfdc012071a65271e2e1c90c6a4b9
thuankhangho <hothuankhang@gmail.com>
2022-09-03 16:06:33 +0700

    Update your_dictionary.dart

--- lib/lib_frontend/your_dictionary.dart
+++ lib/lib_frontend/your_dictionary.dart
@@ -1,27 +1,27 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/models/dictionary_model.dart';
-import 'package:flutter_app/services/database.dart';
+// import 'package:flutter_app/models/dictionary_model.dart';
+// import 'package:flutter_app/services/database.dart';
 import 'custom_search.dart';
 
-final yourDict = Database();
+// final yourDict = Database();
 
 class YourDictionary extends StatelessWidget {
   const YourDictionary({Key? key}) : super(key: key);
 
-  Widget buildDictItems(BuildContext context, DocumentSnapshot snapshot) {
-    final word = DictionaryModel.fromSnapshot(snapshot);
-    return ListTile(
-      title: Text(word.word!),
-    );
-  }
+  // Widget buildDictItems(BuildContext context, DocumentSnapshot snapshot) {
+  //   final word = DictionaryModel.fromSnapshot(snapshot);
+  //   return ListTile(
+  //     title: Text(word.word!),
+  //   );
+  // }
 
-  Widget buildDict(BuildContext context, List<DocumentSnapshot> snapshot) {
-    return ListView(
-        padding: const EdgeInsets.only(top: 20),
-        children:
-            snapshot.map((data) => buildDictItems(context, data)).toList());
-  }
+  // Widget buildDict(BuildContext context, List<DocumentSnapshot> snapshot) {
+  //   return ListView(
+  //       padding: const EdgeInsets.only(top: 20),
+  //       children:
+  //           snapshot.map((data) => buildDictItems(context, data)).toList());
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -90,29 +90,29 @@ class YourDictionary extends StatelessWidget {
                             ),
                             onTap: () => showSearch(
                                 context: context, delegate: CustomSearch())),
-                        StreamBuilder<QuerySnapshot>(
-                          stream: yourDict.getStream(),
-                          builder: ((context, snapshot) {
-                            if (snapshot.hasData == false) {
-                              return const LinearProgressIndicator();
-                            }
-                            return buildDict(
-                                context, snapshot.data?.docs ?? []);
-                          }),
-                        )
-                        //       Container(
-                        //           margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                        //           height: 500,
-                        //           decoration: BoxDecoration(
-                        //             border: Border.all(
-                        //               color: Colors.black,
-                        //               width: 1,
-                        //             ),
-                        //             // borderRadius: BorderRadius.circular(20),
-                        //           ),
-                        //           child: Center(
-                        //               child: Text(
-                        //                   "Chưa có từ vựng được lưu trong thư mục này")))
+                        // StreamBuilder<QuerySnapshot>(
+                        //   stream: yourDict.getStream(),
+                        //   builder: ((context, snapshot) {
+                        //     if (snapshot.hasData == false) {
+                        //       return const LinearProgressIndicator();
+                        //     }
+                        //     return buildDict(
+                        //         context, snapshot.data?.docs ?? []);
+                        //   }),
+                        // )
+                        Container(
+                            margin: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                            height: 500,
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.black,
+                                width: 1,
+                              ),
+                              // borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Center(
+                                child: Text(
+                                    "Chưa có từ vựng được lưu trong thư mục này")))
                       ],
                     ),
                   )),
