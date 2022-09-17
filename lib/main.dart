dd6d0e3069b199e2a111d9e70f9e85b04a292490
thuankhangho <hothuankhang@gmail.com>
2022-09-17 20:37:50 +0700

    Finished 1.2

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/main_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
